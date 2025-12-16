5824370e679bb2f37504e04afd5a336239d9a058
Abishek6702 <abishekkrishnat@gmail.com>
2025-12-16 09:15:34 +0530

    4

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
 const app = express();
-const allowedOrigins =["https://face-login-server.onrender.com","http://localhost:5173"]
+const allowedOrigins =["https://face-login-server.onrender.com","http://localhost:5173","https://face-login-eight.vercel.app"]
 app.use(
   cors({
     origin: function (origin, callback) {
